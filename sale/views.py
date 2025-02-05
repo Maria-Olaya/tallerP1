be7e1f31fa8c31c7962d0f18785a8f9df8b8fc72
Maria-Olaya <mmolayal@eafit.edu.co>
2025-02-05 17:37:27 -0500

    nombre reemplazado

--- sale/views.py
+++ sale/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 def home (request):
     #return HttpResponse('<h1>Welcome to home Page</h1> ')
     #return render(request, 'home.html')
-    return render(request,'home.html', {'name':'Greg Lim'})
+    return render(request,'home.html', {'name':'Maria Olaya'})
 # Create your views here.
